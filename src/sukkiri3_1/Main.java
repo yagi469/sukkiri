326730ae7bbee64f17d81b0f2f9d588c646e3a00
yaginumatti <ryohei122yagi@gmail.com>
2022-09-16 14:04:26 +0900

    create 3_2

--- src/sukkiri3_1/Main.java
+++ src/sukkiri3_1/Main.java
@@ -1,3 +1,5 @@
+package sukkiri3_1;
+
 public class Main {
 	public static void main(String[] args) {
 		boolean tenki = true;
---
+++ src/sukkiri3_2/Main.java
@@ -0,0 +1,11 @@
+package sukkiri3_2;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean doorClose = true; // ここでtrueかfalseを代入
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+}
